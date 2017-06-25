62bf8abec4049499e14e863f44ff0c5b0e0fb74b
Terutomo Takahashi <terutomo.takahashi@gmail.com>
2017-06-25 16:17:57 +0900

    コメント追加

--- opt/test.js
+++ opt/test.js
@@ -1,12 +1,23 @@
+// Sinon.JS初期化
 var sinon = require('sinon');
 var assert = require('assert');
-var jsdom = require('jsdom/lib/old-api.js');
 
+// jQuery初期化
+var jsdom = require('jsdom/lib/old-api.js');
 var window = jsdom.jsdom().defaultView;
 var $ = require('jquery')(window);
 
+/**
+ * User APIのテスト
+ */
 describe('Test User API', function () {
+
+    /**
+     * $.getのテスト
+     * ※callbackの完了を待つ
+     */
     it("get /api/users/100", function (done) {
+        // sinon.stab で$.getを置き換え
         sinon.stub($, 'get').callsFake(function(url, success, dataType) {
             success({
                 id: 100,
@@ -14,20 +25,30 @@ describe('Test User API', function () {
             });
         });
 
+        // テスト
         $.get(
             '/api/users/100',
             function (data) {
+                // Assertion
                 assert.equal(100, data.id);
                 assert.equal('teruchi', data.name);
 
+                // スタブから元に戻す
                 $.get.restore();
+
+                // callback完了をmochaに通知
                 done();
             },
             'json'
         );
     });
 
+    /**
+     * $.ajaxのテスト
+     * ※callbackの完了を待つ
+     */
     it("ajax /api/users/100", function (done) {
+        // sinon.stab で$.ajaxのreturnのdoneを置き換え
         sinon.stub($, 'ajax').returns({
             done : function(callback) {
                 callback({
@@ -37,16 +58,21 @@ describe('Test User API', function () {
             }
         });
 
+        // テスト
         $.ajax({
             method: 'GET',
             url: '/api/users/100',
             dataType: 'json'
         })
         .done(function(data) {
+            // Assertion
             assert.equal(100, data.id);
             assert.equal('teruchi', data.name);
 
+            // スタブから元に戻す
             $.ajax.restore();
+
+            // callback完了をmochaに通知
             done();
         });
     });
